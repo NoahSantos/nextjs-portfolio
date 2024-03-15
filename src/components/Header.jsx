50e1654d2365af1e05936e0ef3fb5eb947b113c2
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-03-15 14:49:29 -0700

    Nav
    Fine tuned the nav to make it smoother

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,83 @@
 "use client"
 import React, { useRef } from "react";
 import { gsap } from "gsap";
+// import { CSSPlugin } from 'gsap/CSSPlugin'
+// gsap.registerPlugin(CSSPlugin)
 import styles from './header.module.css';
 
 export default function Header() {
-    const app = useRef();
+    const bar = useRef();
     let box = useRef([]);
+    let count = useRef(1);
     const pushRef = (el) => box.current.push(el)
 
-    const onEnter = () => {
-        gsap.to(box.current, {
-            duration: 0.5, 
-            x: -50, 
-            stagger: 0.05,
-            ease: "back.in"
-        });
+    const openNav = async() => {
+        if(count.current === 0){
+            await gsap.to(bar.current, {
+            duration: 0.3, 
+            y: -500, 
+            });
+            await gsap.to(box.current, {
+                duration: 0.4, 
+                x: -200, 
+                stagger: 0.025,
+                ease: "back.inOut"
+            });
+            count.current = 1;
+        }else{
+            await gsap.to(box.current, {
+                duration: 0.3, 
+                x: -50, 
+                stagger: 0.025,
+                ease: "back.inOut"
+            });
+            gsap.to(bar.current, {
+                duration: 0.4, 
+                y: 90, 
+            });
+            count.current = 0;
+        }
+
     };
     
-    const onLeave = () => {
-        gsap.to(box.current, {
-            duration: 0.5, 
-            x: -250, 
-            stagger: 0.05,
+    const onEnter = async() => {
+        await gsap.to(box.current, {
+            duration: 0.4, 
+            x: -50, 
+            stagger: 0.025,
+            ease: "back.in"
+        });
+        gsap.to(bar.current, {
+            duration: 0.4, 
+            y: 90, 
+        });
+    };
+
+    const onLeave = async() => {
+        await gsap.to(bar.current, {
+            duration: 0.4, 
+            y: -500, 
+        });
+        await gsap.to(box.current, {
+            duration: 0.4, 
+            x: -200, 
+            stagger: 0.025,
             ease: "back.in"
         });
     };
     
     return (
-        <div ref={app} className={styles.app} onMouseEnter={onEnter} onMouseLeave={onLeave}>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-            <div ref={pushRef} className={styles.box}></div>
-        </div>
+        <>
+            <div className={styles.topBar}>
+
+            </div>
+            <div ref={bar} className={styles.navbar}></div>
+            <div className={styles.app}>
+                {Array(36).fill(0).map((x, i) => (
+                    <div ref={pushRef} className={styles.box} key={i} onClick={openNav}></div>
+                    // <div ref={pushRef} className={styles.box} key={i} onMouseEnter={onEnter} onMouseLeave={onLeave}></div>
+                ))}
+            </div>
+        </>
     );
 }
