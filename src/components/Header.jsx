837e872dfe7686a0ddd2bfe226aa14c3487da25d
Noah-Santos <noahbsantos@gmail.com>
2024-03-21 16:41:41 -0700

    Nav
    Made the nav animation, still need to create the mobile design and make it scalable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,22 @@ import Link from 'next/link'
 
 export default function Header({activeLink}) {
     const bar = useRef();
+    const links = useRef();
     let box = useRef([]);
     let count = useRef(1);
     const pushRef = (el) => box.current.push(el)
 
     const openNav = async() => {
         if(count.current === 0){
+            await gsap.to(links.current, {
+                duration: 0.2, 
+                x: -300, 
+            });
             await gsap.to(bar.current, {
-            duration: 0.3, 
+            duration: 0.2, 
             y: -500, 
             });
-            await gsap.to(box.current, {
+            gsap.to(box.current, {
                 duration: 0.3, 
                 x: -200, 
                 stagger: 0.015,
@@ -28,11 +33,15 @@ export default function Header({activeLink}) {
                 duration: 0.3, 
                 x: -50, 
                 stagger: 0.015,
-                ease: "back.inOut"
+                ease: "back.in"
             });
             gsap.to(bar.current, {
-                duration: 0.4, 
-                y: 90, 
+                duration: 0.2, 
+                y: '10vh', 
+            });
+            gsap.to(links.current, {
+                duration: 0.3, 
+                x: 0, 
             });
             count.current = 0;
         }
@@ -71,7 +80,7 @@ export default function Header({activeLink}) {
 
             </div>
             <div ref={bar} className={styles.navbar}></div>
-            <div className={styles.linkCont}>
+            <div ref={links} className={styles.linkCont}>
                 <ul className={styles.linkList}>
                     {activeLink === 'home' ? <li><Link className={styles.activeList} href="/">Home</Link></li> : <li><Link className={styles.list} href="/">Home</Link></li>}
                     <li><Link className={styles.list} href="/about">About</Link></li>
