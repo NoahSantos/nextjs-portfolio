b9cdd5c0e045b10b60c448e356e8aaaebbc8e371
Noah-Santos <noahbsantos@gmail.com>
2024-04-01 21:14:06 -0700

    Line
    Got the vertical line to appear and grow when the page loads

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,33 @@
 "use client";
-import React from 'react';
+import {useRef, useEffect} from 'react';
 import styles from './Styles/css/home.module.css';
+import { gsap } from "gsap";
 
 export default function Page() {
+  let line = useRef();
+
+  useEffect(() => {
+    gsap.to(line.current, {
+      duration: 0.4, 
+      height: '100%', 
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
-      
+      <div className={styles.infoCont}>
+        <div className={styles.name}>
+          <div className={styles.lineCont}>
+            <div ref={line} className={styles.line}></div>
+          </div>
+        </div>
+
+        <div className={styles.brief}></div>
+      </div>
+
+      <div className={styles.imageCont}>
+
+      </div>
     </div>
   )
 }
