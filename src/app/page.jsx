56913aac762f6fc1c9ec7f8d1072716c3984cc7d
Noah-Santos <noahbsantos@gmail.com>
2024-04-03 17:34:20 -0700

    Positioning
    My name now slides out when the home page loads. Just need to scale the text size.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,30 @@
 "use client";
 import {useRef, useEffect} from 'react';
-import styles from './Styles/css/home.module.css';
+import styles from './Styles/scss/home.module.scss';
 import { gsap } from "gsap";
 
 export default function Page() {
   let line = useRef();
+  let firstN = useRef();
+  let lastN = useRef();
+  let firstNCont = useRef();
+  let lastNCont = useRef();
 
   useEffect(() => {
-    gsap.to(line.current, {
-      duration: 0.4, 
-      height: '100%', 
-    });
+    (async()=>{
+      await gsap.to(line.current, {
+        duration: 0.4, 
+        height: '100%', 
+      });
+      gsap.to(firstN.current, {
+        duration: 0.4, 
+        x: `-=${firstNCont.current.getBoundingClientRect().width}`, 
+      });
+      gsap.to(lastN.current, {
+        duration: 0.4, 
+        x: `+=${lastNCont.current.getBoundingClientRect().width}`, 
+      });
+    })()
   }, []);
 
   return (
@@ -19,11 +33,11 @@ export default function Page() {
         <div className={styles.name}>
           <div className={styles.lineCont}>
             <div ref={line} className={styles.line}></div>
-            <div className={`${styles.box} ${styles.coverBoxF}`}>
-              <p className={`${styles.name} ${styles.fName}`}>Noah</p>
+            <div ref={firstNCont} className={`${styles.box} ${styles.coverBoxF}`}>
+              <p ref={firstN} className={`${styles.nameText} ${styles.fName}`}>Noah</p>
             </div>
-            <div className={`${styles.box} ${styles.coverBoxL}`}>
-              <p className={`${styles.name} ${styles.LName}`}>Santos</p>
+            <div ref={lastNCont} className={`${styles.box} ${styles.coverBoxL}`}>
+              <p ref={lastN} className={`${styles.nameText} ${styles.lName}`}>Santos</p>
             </div>
           </div>
         </div>
