0b1051c8a7c9b23632bab5cee77cec3cf63b8e96
Noah-Santos <noahbsantos@gmail.com>
2024-04-10 21:46:47 -0700

    Scrolling
    The user can scroll on the page without actually cause the page to scroll, and when they scroll, it will rotate the element.

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import styles from '../Styles/css/resume.module.css';
+'use client';
+import styles from '../Styles/scss/resume.module.scss';
+import { useEffect, useState, useRef } from 'react';
 
-export default function Resume () {
+export default function Resume() {
+    const [rotation, setRotation] = useState(0);
+    let temp = useRef();
+
+    const handleWheel = (event) => {
+        const newRotation = rotation + event.deltaY * 0.1;
+        setRotation(newRotation);
+    };
+
+    useEffect(() => {
+        temp.current.style.transform = `rotate(${rotation}deg)`;
+    }, [rotation])
+    
+
     return (
-        <div>page</div>
+        <>
+            <div className={styles.resumeCont} onWheel={handleWheel}>
+                <div ref={temp} className={styles.temp} style={{ transform: `rotate(${rotation}deg)` }}></div>
+            </div>
+        </>
     )
-}
+};
