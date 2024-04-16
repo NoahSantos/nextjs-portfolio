bc45e919b4838aae272bbc19441bffa1ab71759f
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-04-16 15:01:24 -0700

    SCSS
    Converted styling to scss

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,5 @@
 'use client'
+import styles from '../Styles/scss/about.module.scss';
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
@@ -6,6 +7,7 @@ import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 export default function About() {
     const containerRef = useRef(null);
     const panelRefs = useRef([]);
+    let firstCont = useRef(null);
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
@@ -17,10 +19,10 @@ export default function About() {
                 xPercent: -100 * (panels.length - 1),
                 ease: "none",
                 scrollTrigger: {
-                trigger: containerRef.current,
-                pin: true,
-                scrub: 1,
-                end: () => "+=" + containerRef.current.offsetWidth
+                    trigger: containerRef.current,
+                    pin: true,
+                    scrub: 1,
+                    end: () => "+=" + containerRef.current.offsetWidth
                 }
             });
         };
@@ -34,23 +36,20 @@ export default function About() {
     }, []);
 
     return (
-        <div>
-        <div className="firstContainer" style={{backgroundColor: 'red', height:'100vh', width:'100vw'}}>
-            <h1>Testing horizontal scrolling w/ three sections</h1>
-            <h2>First Container</h2>
-        </div>
-        <div className="container" ref={containerRef} style={{width: '400vw', height: '100vh', display: 'flex', flexWrap: 'nowrap'}}>
-            <div className="description panel blue" style={{backgroundColor: 'blue', height:'100vh', width:'100vw'}}>
-            <div>
-                SCROLL DOWN
-                <div className="scroll-down"><div className="arrow"></div></div>
+        <div className={styles.aboutCont}>
+            <div className={styles.firstContainer} ref={firstCont}>
+                <h1>Testing horizontal scrolling w/ three sections</h1>
+                <h2>First Container</h2>
             </div>
+            <div className={styles.container} ref={containerRef}>
+                <div className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>
+                    <div className={styles.first}>SCROLL DOWN</div>
+                </div>
+                <section className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>ONE</section>
+                <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>TWO</section>
+                <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>THREE</section>
             </div>
-            <section className="panel red" ref={el => panelRefs.current.push(el)} style={{backgroundColor: 'orange', height:'100vh', width:'100vw'}}>ONE</section>
-            <section className="panel orange" ref={el => panelRefs.current.push(el)} style={{backgroundColor: 'purple', height:'100vh', width:'100vw'}}>TWO</section>
-            <section className="panel purple" ref={el => panelRefs.current.push(el)} style={{backgroundColor: 'green', height:'100vh', width:'100vw'}}>THREE</section>
-        </div>
-        <div className="lastContainer"  style={{backgroundColor: 'yellow', height:'100vh', width:'100vw'}}>Last Container</div>
+            <div className={styles.lastContainer}>Last Container</div>
         </div>
     );
 }
