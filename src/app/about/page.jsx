a332f2f0f308277bf926be70727f36d422b88892
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-04-17 13:36:12 -0700

    YES
    Got the double horizontal scrolling to finally work. LETS GO

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,55 +3,62 @@ import styles from '../Styles/scss/about.module.scss';
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import Scrolling from '../../components/Scrolling'
 
 export default function About() {
-    const containerRef = useRef(null);
-    const panelRefs = useRef([]);
-    let firstCont = useRef(null);
-    let cont = useRef(null);
+    // const containerRef = useRef(null);
+    // const panelRefs = useRef([]);
+    // let firstCont = useRef(null);
+    // let cont = useRef(null);
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
+    // useEffect(() => {
+    //     gsap.registerPlugin(ScrollTrigger);
 
-        const setupHorizontalScroll = () => {
-            const panels = panelRefs.current;
+    //     const setupHorizontalScroll = () => {
+    //         const panels = panelRefs.current;
 
-            gsap.to(panels, {
-                xPercent: -100 * (panels.length - 1),
-                ease: "none",
-                scrollTrigger: {
-                trigger: containerRef.current,
-                pin: true,
-                scrub: 1,
-                end: () => "+=" + containerRef.current.offsetWidth
-                }
-            });
-        };
+    //         gsap.to(panels, {
+    //             xPercent: -100 * (panels.length - 1),
+    //             ease: "none",
+    //             scrollTrigger: {
+    //             trigger: containerRef.current,
+    //             pin: true,
+    //             scrub: 1,
+    //             end: () => "+=" + containerRef.current.offsetWidth
+    //             }
+    //         });
+    //     };
 
-        setupHorizontalScroll();
+    //     setupHorizontalScroll();
 
-        // Clean up function
-        return () => {
-            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-        };
-    }, []);
+    //     // Clean up function
+    //     return () => {
+    //         ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    //     };
+    // }, []);
+
+    let classes = [styles.one, styles.two]
 
     return (
-        <div className={styles.aboutCont} ref={cont}>
-            <div className={styles.firstContainer} ref={firstCont}>
-                <h1>Testing horizontal scrolling w/ three sections</h1>
-                <h2>First Container</h2>
-            </div>
-            <div className={styles.container} ref={containerRef}>
-                <div className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>
-                    <div className={styles.first}>SCROLL DOWN</div>
-                </div>
-                <section className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>ONE</section>
-                <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>TWO</section>
-                <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>THREE</section>
-            </div>
-            <div className={styles.lastContainer}>Last Container</div>
+        // <div className={styles.aboutCont} ref={cont}>
+        //     <div className={styles.firstContainer} ref={firstCont}>
+        //         <h1>Testing horizontal scrolling w/ three sections</h1>
+        //         <h2>First Container</h2>
+        //     </div>
+        //     <div className={styles.container} ref={containerRef}>
+        //         <div className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>
+        //             <div className={styles.first}>SCROLL DOWN</div>
+        //         </div>
+        //         <section className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>ONE</section>
+        //         <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>TWO</section>
+        //         <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>THREE</section>
+        //     </div>
+        //     <div className={styles.lastContainer}>Last Container</div>
             
+        // </div>
+        <div className={styles.testCont}>
+            <Scrolling classes={classes[0]}></Scrolling>
+            <Scrolling classes={classes[1]}></Scrolling>
         </div>
     );
 }
---
+++ src/components/Scrolling.jsx
@@ -0,0 +1,57 @@
+'use client'
+import styles from '../app/Styles/scss/about.module.scss';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+
+export default function Scrolling({classes}) {
+    const containerRef = useRef(null);
+    const panelRefs = useRef([]);
+    let firstCont = useRef(null);
+    let cont = useRef(null);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        const setupHorizontalScroll = () => {
+            const panels = panelRefs.current;
+
+            gsap.to(panels, {
+                xPercent: -100 * (panels.length - 1),
+                ease: "none",
+                scrollTrigger: {
+                trigger: containerRef.current,
+                pin: true,
+                scrub: 1,
+                end: () => "+=" + containerRef.current.offsetWidth
+                }
+            });
+        };
+
+        setupHorizontalScroll();
+
+        // Clean up function
+        return () => {
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        };
+    }, []);
+
+    return (
+        <div className={`${styles.aboutCont} ${classes}`} ref={cont}>
+            {/* <div className={styles.firstContainer} ref={firstCont}>
+                <h1>Testing horizontal scrolling w/ three sections</h1>
+                <h2>First Container</h2>
+            </div> */}
+            <div className={styles.container} ref={containerRef}>
+                <div className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>
+                    <div className={styles.first}>SCROLL DOWN</div>
+                </div>
+                <section className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>ONE</section>
+                <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>TWO</section>
+                <section  className={`${styles.description} ${styles.panel}`} ref={el => panelRefs.current.push(el)}>THREE</section>
+            </div>
+            <div className={styles.lastContainer}>Last Container</div>
+            
+        </div>
+    );
+}
