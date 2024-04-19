1a050d54bf688d0258d71f275beb2e9710498b46
Noah-Santos <noahbsantos@gmail.com>
2024-04-18 19:28:02 -0700

    Images
    Added the images to the about page

--- src/components/Scrolling.jsx
+++ src/components/Scrolling.jsx
@@ -7,13 +7,16 @@ import Image from 'next/image';
 import intro from '../../public/images/About.JPG';
 import about from '../../public/images/Noah-Chair.jpeg';
 import gaming from '../../public/images/Total War.jpg'
+import history from '../../public/images/History.jpg';
+import song from '../../public/images/Song.jpg';
+import walking from '../../public/images/Walking.jpg';
+import travel from '../../public/images/Travel.jpg'
+import reading from '../../public/images/Reading.JPG';
 
 export default function Scrolling({classes, boxes, section}) {
     const containerRef = useRef(null);
     const panelRefs = useRef([]);
-    let firstCont = useRef(null);
     let cont = useRef(null);
-    let horizontalSections = [];
 
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
@@ -47,13 +50,6 @@ export default function Scrolling({classes, boxes, section}) {
         };
     }, []);
 
-    // for(let i = 0; i < boxes; i++){
-    //     horizontalSections.push(<div className={`${classes[i]} ${styles.panel}`} key={i} ref={el => panelRefs.current.push(el)}>
-    //         <div className={styles.first}>Scroll</div>
-    //     </div>)
-    // }
-
-
     return (
         <div className={`${styles.aboutCont}`} ref={cont}>
             <div className={styles.container} ref={containerRef}>
@@ -73,7 +69,7 @@ export default function Scrolling({classes, boxes, section}) {
                             <p className={styles.title}>About Me</p>
                             <div className={styles.content}>
                                 <div className={styles.imageCont}>
-                                    <Image src={about} alt='picture of me' className={styles.img}></Image>
+                                    {/* <Image src={about} alt='picture of me' className={styles.img}></Image> */}
                                 </div>
                                 <p className={styles.aboutMe}>My name is Noah Santos. I was born and raised here in Arizona. I am a senior in high school and am planning to go to ASU for college.</p>
                             </div>
@@ -94,7 +90,7 @@ export default function Scrolling({classes, boxes, section}) {
                                 <p className={styles.hobbyTitle}>History</p>
                                 <div className={`${styles.hobbyContent} ${styles.hobbyContentR}`}>
                                     <div className={styles.hobbyImgCont}>
-                                        <Image src={gaming} alt='Video Game' className={styles.hobbyImg}></Image>
+                                        <Image src={history} alt='Tank' className={styles.hobbyImg}></Image>
                                     </div>
                                     <p className={`${styles.hobbyDesc} ${styles.hobbyDescR}`}>I really like reading and watching videos on history, specifically war-related history. WW2 is my favorite history period, and I love learning about events from the war.</p>
                                 </div>
@@ -107,7 +103,7 @@ export default function Scrolling({classes, boxes, section}) {
                                 <p className={styles.hobbyTitle}>Reading</p>
                                 <div className={`${styles.hobbyContent} ${styles.hobbyContentL}`}>
                                     <div className={styles.hobbyImgCont}>
-                                        <Image src={gaming} alt='Video Game' className={styles.hobbyImg}></Image>
+                                        <Image src={gaming} alt='Reading' className={`${styles.hobbyImg} ${styles.reading}`}></Image>
                                     </div>
                                     <p className={`${styles.hobbyDesc} ${styles.hobbyDescL}`}>I&apos;ve recently gotten back into reading, and so far, I have read Enders Game, which I enjoyed a lot. I am curently reading some history books on WW2 right now though.</p>
                                 </div>
@@ -116,7 +112,7 @@ export default function Scrolling({classes, boxes, section}) {
                                 <p className={styles.hobbyTitle}>Traveling</p>
                                 <div className={`${styles.hobbyContent} ${styles.hobbyContentR}`}>
                                     <div className={styles.hobbyImgCont}>
-                                        <Image src={gaming} alt='Video Game' className={styles.hobbyImg}></Image>
+                                        <Image src={travel} alt='Travel' className={styles.hobbyImg}></Image>
                                     </div>
                                     <p className={`${styles.hobbyDesc} ${styles.hobbyDescR}`}>I don&apos;t go on vacation as much, but from past experiences, I really enjoyed my trip to Hawaii and Japan.</p>
                                 </div>
@@ -140,7 +136,7 @@ export default function Scrolling({classes, boxes, section}) {
                                 <p className={styles.hobbyTitle}>Music</p>
                                 <div className={`${styles.hobbyContent} ${styles.hobbyContentL}`}>
                                     <div className={styles.hobbyImgCont}>
-                                        <Image src={gaming} alt='Video Game' className={styles.hobbyImg}></Image>
+                                        <Image src={song} alt='Music' className={styles.hobbyImg}></Image>
                                     </div>
                                     <p className={`${styles.hobbyDesc} ${styles.hobbyDescL}`}>I listen to a lot of genres of music like lo-fi, pop, phonk, metal, and instrumental. I generally go with phonk as that is my favorite.</p>
                                 </div>
@@ -149,7 +145,7 @@ export default function Scrolling({classes, boxes, section}) {
                         <p className={styles.hobbyTitle}>Walking</p>
                         <div className={`${styles.hobbyContent} ${styles.hobbyContentR}`}>
                             <div className={styles.hobbyImgCont}>
-                                <Image src={gaming} alt='Video Game' className={styles.hobbyImg}></Image>
+                                <Image src={walking} alt='Walking' className={styles.hobbyImg}></Image>
                             </div>
                             <p className={`${styles.hobbyDesc} ${styles.hobbyDescR}`}>I enjoy going on walks around my community center. It&apos;s fun to look at all the scenery, and it is a great way to clear my head.</p>
                         </div>
