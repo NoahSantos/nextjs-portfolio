6aec0b217233cd4d1d4c4962b296d29fd7d24780
NoahSantos <noahbsantos@gmail.com>
2024-04-21 16:45:57 -0700

    Projects
    Decided to keep the projects page because I am really tired and want to rest. I added the animations and a hover effect. Need to get the images now

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,28 +1,36 @@
 'use client';
+import {useEffect} from 'react';
 import styles from '../Styles/scss/project.module.scss';
 import history from '../../../public/images/History.jpg';
 import Card from '../../components/Card';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Project() {
+    useEffect(() => {
+        AOS.init({})
+    }, [])
+    
+
     return (
         <div className={styles.projectCont}>
             <div className={styles.container}>
-                <Card img={history} title={'AAWL'} link={'https://github.com/Noah-Santos/fbla-website-design.git'}></Card>
-                <Card img={history} title={'Social Media'} link={'https://github.com/Noah-Santos/social-media.git'}></Card>
-                <Card img={history} title={'Active API'} link={'https://github.com/Noah-Santos/Active-Api-Project.git'}></Card>
-                <Card img={history} title={'Gamebox'} link={'https://github.com/Noah-Santos/Gamebox.git'}></Card>
-                <Card img={history} title={'QuikTek'} link={'https://github.com/Noah-Santos/Client-Final.git'}></Card>
-                <Card img={history} title={'Cloud Task Manager'} link={'https://github.com/Noah-Santos/Cloud-Task-Manager.git'}></Card>
-                <Card img={history} title={'Summer'} link={'https://github.com/Noah-Santos/Summer-Assignment.git'}></Card>
-                <Card img={history} title={'To-Do List'} link={'https://github.com/Noah-Santos/to-do-list-mongo.git'}></Card>
-                <Card img={history} title={'JWTs'} link={'https://github.com/Noah-Santos/jwt_challenge2.git'}></Card>
-                <Card img={history} title={'MongoDB'} link={'https://github.com/Noah-Santos/mongodb.git'}></Card>
-                <Card img={history} title={'Theme Switcher'} link={'https://github.com/Noah-Santos/theme-switcher.git'}></Card>
-                <Card img={history} title={'jQuery Profile'} link={'https://github.com/Noah-Santos/jQuery-Profile.git'}></Card>
-                <Card img={history} title={'Rigged Election'} link={'https://github.com/Noah-Santos/Voting-For-Nixon.git'}></Card>
-                <Card img={history} title={'Tic-Tac-Toe'} link={'https://github.com/Noah-Santos/Tic-Tac-Toe.git'}></Card>
-                <Card img={history} title={'Home Page'} link={'https://github.com/Noah-Santos/home-page.git'}></Card>
-                <Card img={history} title={'Birthday'} link={'https://github.com/Noah-Santos/birthday_reminder.git'}></Card>
+                <Card img={history} title={'AAWL'} link={'https://github.com/Noah-Santos/fbla-website-design.git'} animation={"zoom-in-right"}></Card>
+                <Card img={history} title={'Social Media'} link={'https://github.com/Noah-Santos/social-media.git'} animation={"zoom-in-up"}></Card>
+                <Card img={history} title={'Active API'} link={'https://github.com/Noah-Santos/Active-Api-Project.git'} animation={"zoom-in-left"}></Card>
+                <Card img={history} title={'Gamebox'} link={'https://github.com/Noah-Santos/Gamebox.git'} animation={"zoom-in-right"}></Card>
+                <Card img={history} title={'QuikTek'} link={'https://github.com/Noah-Santos/Client-Final.git'} animation={"zoom-in-up"}></Card>
+                <Card img={history} title={'Cloud Task Manager'} link={'https://github.com/Noah-Santos/Cloud-Task-Manager.git'} animation={"zoom-in-left"}></Card>
+                <Card img={history} title={'Summer'} link={'https://github.com/Noah-Santos/Summer-Assignment.git'} animation={"zoom-in-right"}></Card>
+                <Card img={history} title={'To-Do List'} link={'https://github.com/Noah-Santos/to-do-list-mongo.git'} animation={"zoom-in-up"}></Card>
+                <Card img={history} title={'JWTs'} link={'https://github.com/Noah-Santos/jwt_challenge2.git'} animation={"zoom-in-left"}></Card>
+                <Card img={history} title={'MongoDB'} link={'https://github.com/Noah-Santos/mongodb.git'} animation={"zoom-in-right"}></Card>
+                <Card img={history} title={'Theme Switcher'} link={'https://github.com/Noah-Santos/theme-switcher.git'} animation={"zoom-in-up"}></Card>
+                <Card img={history} title={'jQuery Profile'} link={'https://github.com/Noah-Santos/jQuery-Profile.git'} animation={"zoom-in-left"}></Card>
+                <Card img={history} title={'Rigged Election'} link={'https://github.com/Noah-Santos/Voting-For-Nixon.git'} animation={"zoom-in-right"}></Card>
+                <Card img={history} title={'Tic-Tac-Toe'} link={'https://github.com/Noah-Santos/Tic-Tac-Toe.git'} animation={"zoom-in-up"}></Card>
+                <Card img={history} title={'Home Page'} link={'https://github.com/Noah-Santos/home-page.git'} animation={"zoom-in-left"}></Card>
+                <Card img={history} title={'Birthday'} link={'https://github.com/Noah-Santos/birthday_reminder.git'} animation={"zoom-in-up"}></Card>
             </div>
         </div>
     );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import {useEffect} from 'react';
 import styles from '../app/Styles/scss/project.module.scss';
 import Image from 'next/image';
 import Link from 'next/link'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-export default function Card({img, title, link}){
+export default function Card({img, title, link, animation}){
+    useEffect(() => {
+        AOS.init({
+            once: true,
+        })
+    }, [])
+
     return (
-        <div className={`card lg:card-side ${styles.card}`}>
+        <div className={`card lg:card-side ${styles.card}`} data-aos={animation} data-aos-duration="500">
             <figure><Image src={img} className={styles.img} alt='project img'></Image></figure>
             <div className={`card-body ${styles.body}`}>
                 <h2 className={`card-title ${styles.title}`}>{title}</h2>
